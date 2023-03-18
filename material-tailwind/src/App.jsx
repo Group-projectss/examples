5a27c4278b84b67f32a4ca32b589d2e7cba914c0
Ramdan <dadanisme@upi.edu>
2023-03-18 23:33:10 +0700

    add material example

---
+++ material-tailwind/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Content from "./Content";
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen w-full">
+      <Content />
+    </div>
+  );
+}
